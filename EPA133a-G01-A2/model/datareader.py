be7aa3ea308575ae4571109eb1e0e60bd68f2382
roydonders <timonotter@gmail.com>
2024-02-28 11:33:10 +0100

    datareader file start

---
+++ EPA133a-G01-A2/model/datareader.py
@@ -0,0 +1,35 @@
+# ---------------------------------------------------------------
+class DataReader:
+    """
+    The short expl
+
+    Long explanation
+
+    Class Attributes: TODO below is incorrect
+    -----------------
+    step_time: int
+        step_time = 1 # 1 step is 1 min
+
+    path_ids_dict: defaultdict
+        Key: (origin, destination)
+        Value: the shortest path (Infra component IDs) from an origin to a destination
+
+        Since there is only one road in the Demo, the paths are added with the road info;
+        when there is a more complex network layout, the paths need to be managed differently
+
+    sources: list
+        all sources in the network
+
+    sinks: list
+        all sinks in the network
+
+    """
+
+    step_time = 1
+
+    def __init__(self):
+
+        self.path = self.get_path()
+
+    def get_path(self):
+        pass
