b3aed4561d1f05caaa2b6b4eba1c757915250d64
roydonders <timonotter@gmail.com>
2024-03-04 16:17:28 +0100

    models creator nu voor enkele scenario

--- EPA133a-G01-A2/model/scenarios.py
+++ EPA133a-G01-A2/model/scenarios.py
@@ -18,17 +18,17 @@ class ScenarioCreator:
 
     # Warning: hardcoding - should we perhaps read this in from excel file?
     def create_lab2_scenarios(self):
-        s1 = Scenario()
-        s2 = Scenario()
-        s3 = Scenario()
-        s4 = Scenario()
-        s5 = Scenario()
-        s6 = Scenario()
-        s7 = Scenario()
-        s8 = Scenario()
-        s9 = Scenario()
-        s10 = Scenario()
-        scenario_list = [s1,s2,s3,s4,s5,s6,s7,s8,s9,s10]
+        s0 = Scenario(0,0,0,0)
+        # s1 = Scenario()
+        # s2 = Scenario()
+        # s3 = Scenario()
+        # s4 = Scenario()
+        # s5 = Scenario()
+        # s6 = Scenario()
+        # s7 = Scenario()
+        # s8 = Scenario()
+        scenario_list = [s0]
+        # scenario_list = [s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10]
         return scenario_list
 
 
@@ -36,16 +36,11 @@ class ModelsCreator:
 
     # where to place seeds? input or random generator of seeds
     # meegeven dat hij ook een aantal replications kan doen?
-    def __init__(self, num_models, runtime, scenario_list = []):
+    def __init__(self, replications, runtime, seeds, scenario):
         self.runtime = runtime
-        self.N = num_models
-        self.scenarios = scenario_list
-
-
-    def create_models_scenarios(self):
-        # error if num_models is unequal to scenario_list length
-        seed = 0 # should fix this
-        # for all scenarios and numlength create model
+        self.N = replications
+        self.seeds = seeds
+        self.scenario = scenario
 
 
     def create_single_model(self, seed):
