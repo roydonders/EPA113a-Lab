ef1b5f4639ad2a9a2c76d415c541dfb15dd19532
Fred <f6185442514@live.nl>
2024-03-08 17:00:53 +0100

    list of driving times all vehicles prints from 300 ticks for run length as well as total, maar er gaat nog iets niet goed

--- EPA133a-G01-A2/model/model_run.py
+++ EPA133a-G01-A2/model/model_run.py
@@ -20,7 +20,7 @@ from pathlib import Path
 # run time 1000 ticks oude model
 # run_length = 1000
 # set to 10 for a fast run
-run_length = 300
+run_length = 301
 
 # Seeds for the different replications
 seeds = [0000000, 1111111, 2222222, 3333333, 4444444, 5555555, 6666666, 7777777, 8888888, 9999999]
