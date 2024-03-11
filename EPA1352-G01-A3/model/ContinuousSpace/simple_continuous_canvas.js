22b35c65a33e5780f641ab1c1d0cae162cbd9da7
bmjverbeek <b.m.j.verbeek@student.tudelft.nl>
2024-03-11 16:39:14 +0100

    JS files geüpdate

--- EPA1352-G01-A3/model/ContinuousSpace/simple_continuous_canvas.js
+++ EPA1352-G01-A3/model/ContinuousSpace/simple_continuous_canvas.js
@@ -1,111 +1,119 @@
-var ContinuousVisualization = function(height, width, context) {
-	var height = height;
-	var width = width;
-	var context = context;
+var ContinuousVisualization = function (height, width, context) {
+  var height = height;
+  var width = width;
+  var context = context;
 
-	this.draw = function(objects) {
-		for (var i in objects) {
-			var p = objects[i];
-			if (p.Shape == "rect")
-				this.drawRectangle(p.x, p.y, p.w, p.h, p.Color, p.Filled, p.Text, p.Text_color);
-			if (p.Shape == "circle")
-				this.drawCircle(p.x, p.y, p.r, p.Color, p.Filled, p.Text, p.Text_color);
-		};
-	};
+  this.draw = function (objects) {
+    for (var i in objects) {
+      var p = objects[i];
+      if (p.Shape == "rect") this.drawRectangle(p.x, p.y, p.w, p.h, p.Color, p.Filled, p.Text, p.Text_color);
+      if (p.Shape == "circle") this.drawCircle(p.x, p.y, p.r, p.Color, p.Filled, p.Text, p.Text_color);
+    }
+  };
 
-	this.drawCircle = function(x, y, radius, color, fill, text, text_color) {
+  this.drawCircle = function (x, y, radius, color, fill, text, text_color) {
+    var cx = x * width;
+    var cy = y * height;
+    var r = radius;
 
-		var cx = x * width;
-		var cy = y * height;
-		var r = radius;
+    context.beginPath();
+    context.arc(cx, cy, r, 0, Math.PI * 2, false);
+    context.closePath();
 
-		context.beginPath();
-		context.arc(cx, cy, r, 0, Math.PI * 2, false);
-		context.closePath();
+    context.strokeStyle = color;
+    context.stroke();
 
-		context.strokeStyle = color;
-		context.stroke();
+    if (fill) {
+      context.fillStyle = color;
+      context.fill();
+    }
 
-		if (fill) {
-			context.fillStyle = color;
-			context.fill();
-		}
+    // This part draws the text inside the Circle
+    if (text !== undefined) {
+      context.fillStyle = text_color;
+      context.textAlign = "center";
+      context.textBaseline = "middle";
+      context.fillText(text, cx, cy);
+    }
+  };
 
-		// This part draws the text inside the Circle
-        if (text !== undefined) {
-                context.fillStyle = text_color;
-                context.textAlign = 'center';
-                context.textBaseline= 'middle';
-                context.fillText(text, cx, cy);
-        }
+  this.drawRectangle = function (x, y, w, h, color, fill, text, text_color) {
+    var cx = x * width;
+    var cy = y * height;
+    var dx = w;
+    var dy = h;
 
-	};
+    // Keep the drawing centered:
+    var x0 = cx - dx * 0.5;
+    var y0 = cy - dy * 0.5;
 
-	this.drawRectangle = function(x, y, w, h, color, fill, text, text_color) {
+    context.beginPath();
+    context.strokeStyle = color;
+    context.fillStyle = color;
 
-		var cx = x * width;
-		var cy = y * height;
-		var dx = w;
-		var dy = h;
+    if (fill) context.fillRect(x0, y0, dx, dy);
+    else context.strokeRect(x0, y0, dx, dy);
 
-		// Keep the drawing centered:
-		var x0 = cx - dx * 0.5;
-		var y0 = cy - dy * 0.5;
+    // This part draws the text inside the Rectangle
+    if (text !== undefined) {
+      context.fillStyle = text_color;
+      context.textAlign = "center";
+      context.textBaseline = "middle";
+      context.fillText(text, cx, cy);
+    }
+  };
 
-		context.beginPath();
-		context.strokeStyle = color;
-		context.fillStyle = color;
-
-		if (fill)
-			context.fillRect(x0, y0, dx, dy);
-		else
-			context.strokeRect(x0, y0, dx, dy);
-
-		// This part draws the text inside the Rectangle
-        if (text !== undefined) {
-                context.fillStyle = text_color;
-                context.textAlign = 'center';
-                context.textBaseline= 'middle';
-                context.fillText(text, cx, cy);
-        }
-	};
-
-	this.resetCanvas = function() {
-		context.clearRect(0, 0, height, width);
-		context.beginPath();
-	};
+  this.resetCanvas = function () {
+    context.clearRect(0, 0, height, width);
+    context.beginPath();
+  };
 };
 
-var Simple_Continuous_Module = function(canvas_width, canvas_height) {
-	// Create the element
-	// ------------------
+var Simple_Continuous_Module = function (canvas_width, canvas_height) {
+  // Create the element
+  // ------------------
 
-	// Create the tag:
-	var canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
-	canvas_tag += "style='border:1px dotted'></canvas>";
-	
-	var parent_div_tag = '<div style="height:' + canvas_height + 'px;" class="world-grid-parent" target="_sim"></div>'
-	
-	// Append it to body:
-	var canvas = $(canvas_tag)[0];
-	
-	var parent = $(parent_div_tag)[0];
-	
-	//$("body").append(canvas);
-	$("#elements").append(parent);
-	parent.append(canvas);
+  // ORIGINAL CODE:
+  // // Create the tag:
+  // var canvas_tag = "<canvas width='" + canvas_width + "' height='" + canvas_height + "' ";
+  // canvas_tag += "style='border:1px dotted'></canvas>";
+  // var parent_div_tag = '<div id="parent" style="height:' + canvas_height + 'px;" class="world-grid-parent" target="_sim"></div>'
+  // // Append it to body:
+  // var canvas = $(canvas_tag)[0];
+  // var parent = $(parent_div_tag)[0];
+  // //$("body").append(canvas);
+  // $("#elements").append(parent);
+  // parent.append(canvas);
 
-	// Create the context and the drawing controller:
-	var context = canvas.getContext("2d");
-	var canvasDraw = new ContinuousVisualization(canvas_width, canvas_height, context);
+  // FIXED CODE (If you are getting Javascript '$ not defined' error)
+  // Create Canvas object
+  const canvas = document.createElement("canvas");
+  canvas.setAttribute("width", canvas_width);
+  canvas.setAttribute("height", canvas_height);
+  canvas.setAttribute("style", "border:1px dotted");
 
-	this.render = function(data) {
-		canvasDraw.resetCanvas();
-		canvasDraw.draw(data);
-	};
+  // Create Parent object
+  const parent_div = document.createElement("div");
+  let parent_style = "height:" + canvas_height + "px;";
+  parent_div.setAttribute("style", parent_style);
+  parent_div.setAttribute("class", "world-grid-parent");
+  parent_div.setAttribute("target", "_sim");
 
-	this.reset = function() {
-		canvasDraw.resetCanvas();
-	};
+  // Add both to document
+  let elements_div = document.getElementById("elements");
+  elements_div.appendChild(parent_div);
+  parent_div.appendChild(canvas);
 
+  // Create the context and the drawing controller:
+  var context = canvas.getContext("2d");
+  var canvasDraw = new ContinuousVisualization(canvas_width, canvas_height, context);
+
+  this.render = function (data) {
+    canvasDraw.resetCanvas();
+    canvasDraw.draw(data);
+  };
+
+  this.reset = function () {
+    canvasDraw.resetCanvas();
+  };
 };
